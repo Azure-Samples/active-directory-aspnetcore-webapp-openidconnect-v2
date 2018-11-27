a4dea8e8c95dbc83fc6d09618e6d7012b94df0a7
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-11-27 16:37:35 +0100

    Improving the AadIssuerValidator to support tokens issued by both the V1 and V2 endpoint.

--- Extensions/AadIssuerValidator.cs
+++ Extensions/AadIssuerValidator.cs
@@ -10,14 +10,16 @@ namespace Microsoft.AspNetCore.Authentication
     public static class AadIssuerValidator
     {
         /// <summary>
-        /// Validate the issuer.
+        /// Validate the issuer for multi-tenant applications of various audience (Work and School account, or Work and School accounts +
+        /// Personal accounts)
         /// </summary>
         /// <param name="issuer">Issuer to validate (will be tenanted)</param>
         /// <param name="securityToken">Received Security Token</param>
         /// <param name="validationParameters">Token Validation parameters</param>
         /// <remarks>The issuer is considered as valid if it has the same http scheme and authority as the
         /// authority from the configuration file, has a tenant Id, and optionally v2.0 (this web api
-        /// accepts both V1 and V2 tokens)</remarks>
+        /// accepts both V1 and V2 tokens).
+        /// Authority aliasing is also taken into account</remarks>
         /// <returns>The <c>issuer</c> if it's valid, or otherwise <c>SecurityTokenInvalidIssuerException</c> is thrown</returns>
         public static string ValidateAadIssuer(string issuer, SecurityToken securityToken, TokenValidationParameters validationParameters)
         {
@@ -48,6 +50,12 @@ namespace Microsoft.AspNetCore.Authentication
             {
                 allValidIssuers.Add(TenantedIssuer(validIssuer, tenantId));
             }
+
+            // Consider the aliases (https://login.microsoftonline.com (v2.0 tokens) => https://sts.windows.net (v1.0 tokens) )
+            allValidIssuers.AddRange(allValidIssuers.Select(i => i.Replace("https://login.microsoftonline.com", "https://sts.windows.net")).ToArray());
+
+            // Consider tokens provided both by v1.0 and v2.0 issuers
+            allValidIssuers.AddRange(allValidIssuers.Select(i => i.Replace("/v2.0", "/")).ToArray());
 
             if (!allValidIssuers.Contains(issuer))
             {
