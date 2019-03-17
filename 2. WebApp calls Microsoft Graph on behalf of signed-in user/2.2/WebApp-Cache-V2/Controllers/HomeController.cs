50359351c6cbaed30867286103c23904ff761ef4
Kalyan Krishna <kalyan.krishna@microsoft.com>
2019-03-17 01:24:30 -0700

    New solution created

---
+++ 2. WebApp calls Microsoft Graph on behalf of signed-in user/2.2/WebApp-Cache-V2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp_OpenIDConnect_DotNet_Cache_V2.Models;
+
+namespace WebApp_OpenIDConnect_DotNet_Cache_V2.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 2. WebApp calls Microsoft Graph on behalf of signed-in user/2.2/WebApp-Cache-V2/Infrastructure/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_OpenIDConnect_DotNet_Cache_V2.Infrastructure
+{
+    public static class Constants
+    {
+        public const string ScopeUserRead = "User.Read";
+        public const string BearerAuthorizationScheme = "Bearer";
+    }
+}
