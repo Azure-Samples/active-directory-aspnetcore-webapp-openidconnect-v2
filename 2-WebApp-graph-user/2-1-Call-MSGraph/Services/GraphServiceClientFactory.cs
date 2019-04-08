a88de548125e9096bd7becec9f5a514ee824f675
Irvine Sunday <irvine.sunday@m-kopa.com>
2019-04-08 13:05:58 +0300

    Adding Microsoft.Graph lib. and factory class for creating authenticated Graph clients

---
+++ 2-WebApp-graph-user/2-1-Call-MSGraph/Services/GraphServiceClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebApp_OpenIDConnect_DotNet.Services
+{
+    public class GraphServiceClientFactory
+    {
+        public static async Task<GraphServiceClient> GetAuthenticatedGraphClient(Func<Task<string>> acquireAccessToken)
+        {
+            // Fetch the access token
+            string accessToken = await acquireAccessToken.Invoke();
+
+            return new GraphServiceClient(new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
+                            Infrastructure.Constants.BearerAuthorizationScheme, accessToken);
+                    }));
+        }
+    }
+}
