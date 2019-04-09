3d7e02c39aab76a8af9e1ea60af3ee27ee61a2a1
Neha Goel <38259657+negoe@users.noreply.github.com>
2019-04-08 23:46:30 -0700

    MS Graph folder for Sovereign scenarios

---
+++ 2-WebApp-graph-user/2-4-Sovereign-Call-MSGraph/Infrastructure/Constants.cs
@@ -0,0 +1,8 @@
+namespace WebApp_OpenIDConnect_DotNet.Infrastructure
+{
+    public static class Constants
+    {
+        public const string ScopeUserRead = "User.Read";
+        public const string BearerAuthorizationScheme = "Bearer";
+    }
+}
