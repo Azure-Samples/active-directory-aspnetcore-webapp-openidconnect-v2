0aef6f10839648ba37cfed545b9202145bd99bcd
Kalyan Krishna <kalyan.krishna@microsoft.com>
2019-04-10 23:49:05 -0700

    Copied from 2-1-Call-MSGraph (#90)

---
+++ 5-WebApp-AuthZ/Roles/Services/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp_OpenIDConnect_DotNet.Services
+{
+    public static class Bootstrapper
+    {
+        public static void AddGraphService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<WebOptions>(configuration);
+        }
+    }
+}
