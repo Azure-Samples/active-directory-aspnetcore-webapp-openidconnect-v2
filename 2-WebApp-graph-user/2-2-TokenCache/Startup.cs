45baf51fa99b5999020b6b1614684853367533a2
Tiago Brenck <v-tibre@microsoft.com>
2019-08-15 15:39:23 -0700

    Removing, `JwtSecurityTokenHandler.DefaultMapInboundClaims = false;` because `preferred_username` claim was not being mapped

--- 2-WebApp-graph-user/2-2-TokenCache/Startup.cs
+++ 2-WebApp-graph-user/2-2-TokenCache/Startup.cs
@@ -33,12 +33,6 @@ namespace WebApp_OpenIDConnect_DotNet
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            // This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
-            // By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
-            // 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles'
-            // This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
-            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-
             // Uncomment the following to initialize the sql server database with tables required to cache tokens.
             // NOTE : This is a one time use method. We advise using it in development environments to create the tables required to enable token caching.
             // For production deployments, preferably, generate the schema from the tables generated in dev environments and use it to create the necessary tables in production.
