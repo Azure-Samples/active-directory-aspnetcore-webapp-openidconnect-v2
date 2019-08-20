83f12160574151fde33e9b4f00fc9bbe4db14444
Tiago Brenck <v-tibre@microsoft.com>
2019-08-20 14:20:47 -0700

    Revert "Removing, `JwtSecurityTokenHandler.DefaultMapInboundClaims = false;` because `preferred_username` claim was not being mapped"
    This reverts commit 45baf51fa99b5999020b6b1614684853367533a2.

--- 4-WebApp-your-API/Client/Startup.cs
+++ 4-WebApp-your-API/Client/Startup.cs
@@ -36,6 +36,12 @@ namespace WebApp_OpenIDConnect_DotNet
 
             services.AddOptions();
 
+            // This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
+            // By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
+            // 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles'
+            // This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
             // Token acquisition service based on MSAL.NET
             // and chosen token cache implementation
             services.AddAzureAdV2Authentication(Configuration)
