1a690ff3d2a1aea03584dff947c811314634bfa5
Nick Moores <NickMoores@users.noreply.github.com>
2019-12-17 16:24:56 +0000

    Removed documentation for "isAppTokenCache"
    Removed documentation for a parameter that no longer exists on a method on the IMsalTokenCacheProvider interface.

--- Microsoft.Identity.Web/TokenCacheProviders/IMsalTokenCacheProvider .cs
+++ Microsoft.Identity.Web/TokenCacheProviders/IMsalTokenCacheProvider .cs
@@ -15,8 +15,6 @@ namespace Microsoft.Identity.Web.TokenCacheProviders
         /// Initializes a token cache (which can be a user token cache or an app token cache)
         /// </summary>
         /// <param name="tokenCache">Token cache for which to initialize the serialization</param>
-        /// <param name="isAppTokenCache">Is the token cache an App token cache or
-        /// a user token cache</param>
         /// <returns></returns>
         Task InitializeAsync(ITokenCache tokenCache);
 
