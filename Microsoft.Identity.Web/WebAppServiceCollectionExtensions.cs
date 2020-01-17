2ed515886e3cb560d0539fe6c5098e3a23c6ca6b
Tiago Brenck <v-tibre@microsoft.com>
2020-01-17 10:22:02 -0800

    Commented non-working method

--- Microsoft.Identity.Web/WebAppServiceCollectionExtensions.cs
+++ Microsoft.Identity.Web/WebAppServiceCollectionExtensions.cs
@@ -31,86 +31,86 @@ namespace Microsoft.Identity.Web
         /// Set to true if you want to debug, or just understand the OpenIdConnect events.
         /// </param>
         /// <returns></returns>
-        public static IServiceCollection AddSignIn(
-            this IServiceCollection services,
-            IConfiguration configuration,
-            string configSectionName = "AzureAD",
-            bool subscribeToOpenIdConnectMiddlewareDiagnosticsEvents = false)
-        {
+        //public static IServiceCollection AddSignIn(
+        //    this IServiceCollection services,
+        //    IConfiguration configuration,
+        //    string configSectionName = "AzureAD",
+        //    bool subscribeToOpenIdConnectMiddlewareDiagnosticsEvents = false)
+        //{
 
-            services.AddAuthentication(auth =>
-            {
-                auth.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                auth.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                auth.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            })
-            .AddCookie()
-            .AddOpenIdConnect(options =>
-            {
-                configuration.GetSection(configSectionName).Bind(options);
+        //    services.AddAuthentication(auth =>
+        //    {
+        //        auth.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        //        auth.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        //        auth.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        //    })
+        //    .AddCookie()
+        //    .AddOpenIdConnect(options =>
+        //    {
+        //        configuration.GetSection(configSectionName).Bind(options);
 
-                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        //        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
-                // Per the code below, this application signs in users in any Work and School
-                // accounts and any Microsoft Personal Accounts.
-                // If you want to direct Azure AD to restrict the users that can sign-in, change
-                // the tenant value of the appsettings.json file in the following way:
-                // - only Work and School accounts => 'organizations'
-                // - only Microsoft Personal accounts => 'consumers'
-                // - Work and School and Personal accounts => 'common'
-                // If you want to restrict the users that can sign-in to only one tenant
-                // set the tenant value in the appsettings.json file to the tenant ID
-                // or domain of this organization
-                options.Authority = options.Authority + "/v2.0/";
+        //        // Per the code below, this application signs in users in any Work and School
+        //        // accounts and any Microsoft Personal Accounts.
+        //        // If you want to direct Azure AD to restrict the users that can sign-in, change
+        //        // the tenant value of the appsettings.json file in the following way:
+        //        // - only Work and School accounts => 'organizations'
+        //        // - only Microsoft Personal accounts => 'consumers'
+        //        // - Work and School and Personal accounts => 'common'
+        //        // If you want to restrict the users that can sign-in to only one tenant
+        //        // set the tenant value in the appsettings.json file to the tenant ID
+        //        // or domain of this organization
+        //        options.Authority = options.Authority + "/v2.0/";
 
-                // If you want to restrict the users that can sign-in to several organizations
-                // Set the tenant value in the appsettings.json file to 'organizations', and add the
-                // issuers you want to accept to options.TokenValidationParameters.ValidIssuers collection
-                options.TokenValidationParameters.IssuerValidator = AadIssuerValidator.GetIssuerValidator(options.Authority).Validate;
+        //        // If you want to restrict the users that can sign-in to several organizations
+        //        // Set the tenant value in the appsettings.json file to 'organizations', and add the
+        //        // issuers you want to accept to options.TokenValidationParameters.ValidIssuers collection
+        //        options.TokenValidationParameters.IssuerValidator = AadIssuerValidator.GetIssuerValidator(options.Authority).Validate;
 
-                // Set the nameClaimType to be preferred_username.
-                // This change is needed because certain token claims from Azure AD V1 endpoint
-                // (on which the original .NET core template is based) are different than Microsoft identity platform endpoint.
-                // For more details see [ID Tokens](https://docs.microsoft.com/azure/active-directory/develop/id-tokens)
-                // and [Access Tokens](https://docs.microsoft.com/azure/active-directory/develop/access-tokens)
-                options.TokenValidationParameters.NameClaimType = "preferred_username";
+        //        // Set the nameClaimType to be preferred_username.
+        //        // This change is needed because certain token claims from Azure AD V1 endpoint
+        //        // (on which the original .NET core template is based) are different than Microsoft identity platform endpoint.
+        //        // For more details see [ID Tokens](https://docs.microsoft.com/azure/active-directory/develop/id-tokens)
+        //        // and [Access Tokens](https://docs.microsoft.com/azure/active-directory/develop/access-tokens)
+        //        options.TokenValidationParameters.NameClaimType = "preferred_username";
 
-                // Avoids having users being presented the select account dialog when they are already signed-in
-                // for instance when going through incremental consent
-                options.Events.OnRedirectToIdentityProvider = context =>
-                {
-                    var login = context.Properties.GetParameter<string>(OpenIdConnectParameterNames.LoginHint);
-                    if (!string.IsNullOrWhiteSpace(login))
-                    {
-                        context.ProtocolMessage.LoginHint = login;
-                        context.ProtocolMessage.DomainHint = context.Properties.GetParameter<string>(
-                            OpenIdConnectParameterNames.DomainHint);
+        //        // Avoids having users being presented the select account dialog when they are already signed-in
+        //        // for instance when going through incremental consent
+        //        options.Events.OnRedirectToIdentityProvider = context =>
+        //        {
+        //            var login = context.Properties.GetParameter<string>(OpenIdConnectParameterNames.LoginHint);
+        //            if (!string.IsNullOrWhiteSpace(login))
+        //            {
+        //                context.ProtocolMessage.LoginHint = login;
+        //                context.ProtocolMessage.DomainHint = context.Properties.GetParameter<string>(
+        //                    OpenIdConnectParameterNames.DomainHint);
 
-                        // delete the login_hint and domainHint from the Properties when we are done otherwise
-                        // it will take up extra space in the cookie.
-                        context.Properties.Parameters.Remove(OpenIdConnectParameterNames.LoginHint);
-                        context.Properties.Parameters.Remove(OpenIdConnectParameterNames.DomainHint);
-                    }
+        //                // delete the login_hint and domainHint from the Properties when we are done otherwise
+        //                // it will take up extra space in the cookie.
+        //                context.Properties.Parameters.Remove(OpenIdConnectParameterNames.LoginHint);
+        //                context.Properties.Parameters.Remove(OpenIdConnectParameterNames.DomainHint);
+        //            }
 
-                    // Additional claims
-                    if (context.Properties.Items.ContainsKey(OidcConstants.AdditionalClaims))
-                    {
-                        context.ProtocolMessage.SetParameter(
-                            OidcConstants.AdditionalClaims,
-                            context.Properties.Items[OidcConstants.AdditionalClaims]);
-                    }
+        //            // Additional claims
+        //            if (context.Properties.Items.ContainsKey(OidcConstants.AdditionalClaims))
+        //            {
+        //                context.ProtocolMessage.SetParameter(
+        //                    OidcConstants.AdditionalClaims,
+        //                    context.Properties.Items[OidcConstants.AdditionalClaims]);
+        //            }
 
-                    return Task.FromResult(0);
-                };
+        //            return Task.FromResult(0);
+        //        };
 
-                if (subscribeToOpenIdConnectMiddlewareDiagnosticsEvents)
-                {
-                    OpenIdConnectMiddlewareDiagnostics.Subscribe(options.Events);
-                }
-            });
+        //        if (subscribeToOpenIdConnectMiddlewareDiagnosticsEvents)
+        //        {
+        //            OpenIdConnectMiddlewareDiagnostics.Subscribe(options.Events);
+        //        }
+        //    });
 
-            return services;
-        }
+        //    return services;
+        //}
 
         // TODO: pass an option with a section name to bind the options ? or a delegate?
 
