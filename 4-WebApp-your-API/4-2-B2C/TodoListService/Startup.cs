050566600c2ed1cf9061cd19c20a9a01b19577f1
Tiago Brenck <v-tibre@microsoft.com>
2020-02-18 11:20:42 -0800

    - Cleaned appsettings
    - Removed DisallowsSameSiteNone from WebAppServiceCollectionExtensions because it got moved to another class
    - Added edit profile button on login bar
    - Fixed merge on AuthorizeForScopesAttribute
    - Added options.TokenValidationParameters.NameClaimType = "name" on web api Startup to fix todo list

--- 4-WebApp-your-API/4-2-B2C/TodoListService/Startup.cs
+++ 4-WebApp-your-API/4-2-B2C/TodoListService/Startup.cs
@@ -31,7 +31,12 @@ namespace TodoListService
 
             // Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddProtectedWebApi("AzureAdB2C", Configuration, options => Configuration.Bind("AzureAdB2C", options));
+                .AddProtectedWebApi("AzureAdB2C", Configuration, options =>
+                {
+                    Configuration.Bind("AzureAdB2C", options);
+
+                    options.TokenValidationParameters.NameClaimType = "name";
+                });
 
             services.AddControllers();
         }
