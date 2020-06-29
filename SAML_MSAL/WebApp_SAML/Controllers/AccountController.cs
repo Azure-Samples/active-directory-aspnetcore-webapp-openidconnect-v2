9bea642612bcc28305cf31bbdab2c3bb2b5e610a
Tiago Brenck <v-tibre@microsoft.com>
2020-06-29 11:09:45 -0700

    SAML MSAL placeholder

---
+++ SAML_MSAL/WebApp_SAML/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Microsoft.Owin.Security;
+
+namespace WebApp_SAML.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, 
+                    WsFederationAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public void SignOut()
+        {
+            string callbackUrl = Url.Action("SignOutCallback", "Account", routeValues: null, protocol: Request.Url.Scheme);
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                WsFederationAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        public ActionResult SignOutCallback()
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SAML_MSAL/WebApp_SAML/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_SAML.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var app = PublicClientApplicationBuilder.Create("992f2eec-20ec-4eb4-952b-2d974f0db6ea")
+                .WithAuthority("https://login.microsoftonline.com/979f4440-75dc-4664-b2e1-2cafa0ac67d1/v2.0")
+                .Build();
+
+            // Set public client true
+            // Grant consent via portal
+
+            var securePassword = new SecureString();
+            foreach (char c in "Vancouver1")        
+                securePassword.AppendChar(c);
+            
+            var result = await app.AcquireTokenByUsernamePassword(new string[] { "User.Read" }, "test1@lab.cxpaadtenant.com", securePassword)
+                .ExecuteAsync().ConfigureAwait(false);
+
+            var userClaims = ClaimsPrincipal.Current.Claims.ToList();
+            return View(userClaims);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
