a71bfb73e3c6d20b9ea9277abb90dd5ab686f04a
damienbod <damien_bod@hotmail.com>
2020-10-09 10:51:30 +0200

    fix merge

--- 2-WebApp-graph-user/2-4-Sovereign-Call-MSGraph/Startup.cs
+++ 2-WebApp-graph-user/2-4-Sovereign-Call-MSGraph/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Identity.Web;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Constants = WebApp_OpenIDConnect_DotNet.Infrastructure.Constants;
 
 namespace WebApp_OpenIDConnect_DotNet
 {
