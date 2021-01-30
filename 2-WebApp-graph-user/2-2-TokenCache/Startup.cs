5207e08539b8354e44bf8338e3b8e4d7e0ae7007
Jean-Marc Prieur <jmprieur@microsoft.com>
2021-01-30 13:56:47 +0100

    Updarting the comment

--- 2-WebApp-graph-user/2-2-TokenCache/Startup.cs
+++ 2-WebApp-graph-user/2-2-TokenCache/Startup.cs
@@ -58,7 +58,9 @@ namespace _2_1_Call_MSGraph
                 options.TableName = "TokenCache";
 
                 // You don't want the SQL token cache to be purged before the access token has expired. Usually
-                // access tokens expire after 1 hours (but this can be changed by token lifetime policies)
+                // access tokens expire after 1 hours (but this can be changed by token lifetime policies), whereas
+                // the default sliding expiration for the distributed SQL database is 20 mins. 
+                // Use a value which is above 60 mins (or the lifetime of a token in case of longer lived tokens)
                 options.DefaultSlidingExpiration = TimeSpan.FromMinutes(90);
             });
 
