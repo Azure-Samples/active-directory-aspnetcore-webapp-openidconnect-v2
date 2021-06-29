5ff1e47a5d1cce4d236b1eb693bdc04c6435ba68
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2021-06-29 15:23:00 +0300

    Update Startup.cs (#507)
    Hello, @jmprieur I would like to test named-snippet (https://review.docs.microsoft.com/en-us/help/contribute/code-in-docs?branch=master#named-snippet). I have proposed a changed to be able to test the named-snippet. Have a look at the proposed change and let me know what is your view.

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace WebApp_OpenIDConnect_DotNet
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // <Configure_service>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
@@ -42,6 +43,7 @@ namespace WebApp_OpenIDConnect_DotNet
            services.AddRazorPages()
                 .AddMicrosoftIdentityUI();
         }
+        // </ Configure_service >
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
