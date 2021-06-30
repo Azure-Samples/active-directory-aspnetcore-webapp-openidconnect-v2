6dddcf233280a9308738b2f69d982d928a45f745
Alexander Beyderman <v-abeyderman@microsoft.com>
2021-06-30 14:31:07 -0400

    added some personal middleware for studies

---
+++ 1-WebApp-OIDC/1-1-MyOrg/MyMiddleware/MiddlewareSimple.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMiddleware
+{
+    public class MiddlewareSimple
+    {
+        private RequestDelegate _next;
+
+        public MiddlewareSimple(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Debug.WriteLine("Incoming request!");
+            await _next.Invoke(context);
+            Debug.WriteLine("Outgoing response!");
+        }
+    }
+
+    public class MyMiddlewareWrapper
+    {
+        public void Configure(IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<MiddlewareSimple>();
+        }
+    }
+}
