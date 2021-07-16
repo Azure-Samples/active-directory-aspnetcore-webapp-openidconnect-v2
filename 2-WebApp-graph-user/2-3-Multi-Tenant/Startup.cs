f90779bcd056a5078b770891260fab4ed6df4f4e
Alexander Beyderman <v-abeyderman@microsoft.com>
2021-07-16 10:07:06 -0400

    cleanup

--- 2-WebApp-graph-user/2-3-Multi-Tenant/Startup.cs
+++ 2-WebApp-graph-user/2-3-Multi-Tenant/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
