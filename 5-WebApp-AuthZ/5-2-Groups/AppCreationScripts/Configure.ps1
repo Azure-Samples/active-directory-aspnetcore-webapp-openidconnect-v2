011e136ff92689eaacefde0da4e2f65139556207
Alexander Beyderman <v-abeyderman@microsoft.com>
2021-10-19 18:06:54 -0400

    update for sample 5-2

--- 5-WebApp-AuthZ/5-2-Groups/AppCreationScripts/Configure.ps1
+++ 5-WebApp-AuthZ/5-2-Groups/AppCreationScripts/Configure.ps1
@@ -233,7 +233,7 @@ Function ConfigureApplications
    # Add Required Resources Access (from 'webApp' to 'Microsoft Graph')
    Write-Host "Getting access from 'webApp' to 'Microsoft Graph'"
    $requiredPermissions = GetRequiredPermissions -applicationDisplayName "Microsoft Graph" `
-                                                -requiredDelegatedPermissions "User.Read|GroupMember.Read.All" `
+                                                -requiredDelegatedPermissions "User.Read" `
 
    $requiredResourcesAccess.Add($requiredPermissions)
 
