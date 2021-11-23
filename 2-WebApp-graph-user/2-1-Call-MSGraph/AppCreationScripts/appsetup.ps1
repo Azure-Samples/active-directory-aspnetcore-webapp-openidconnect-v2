4027b5aa62f8ba01420c05d13d0d183a2bf06eec
Alexander Beyderman <v-abeyderman@microsoft.com>
2021-11-23 13:35:18 -0500

    save

---
+++ 2-WebApp-graph-user/2-1-Call-MSGraph/AppCreationScripts/appsetup.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+$secpasswd = ConvertTo-SecureString "Abeyd@123" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("usr@adstudies.onmicrosoft.com", $secpasswd)
+$tenantId = "46f4b84b-469c-437b-a082-81d712b34232"
+
+.\Cleanup.ps1 -Credential $mycreds -TenantId $tenantId
+.\Configure.ps1 -Credential $mycreds -TenantId $tenantId
