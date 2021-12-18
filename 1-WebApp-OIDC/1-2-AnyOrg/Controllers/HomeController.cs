10caeb7ae16bdd6c3dbdeae53d11e9e99303f005
Gen Cai <gencai@microsoft.com>
2021-12-18 20:56:40 +0800

    update HomeController

--- 1-WebApp-OIDC/1-2-AnyOrg/Controllers/HomeController.cs
+++ 1-WebApp-OIDC/1-2-AnyOrg/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace WebApp_OpenIDConnect_DotNet.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
