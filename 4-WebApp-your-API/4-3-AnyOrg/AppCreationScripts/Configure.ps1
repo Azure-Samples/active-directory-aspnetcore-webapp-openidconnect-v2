e546790ce1914a2eaa0c311dc9f1f621f301cf86
Alexander Beyderman <v-abeyderman@microsoft.com>
2022-02-14 14:49:49 -0500

    reverted to back compatible URL names in sample.json + cleanup

--- 4-WebApp-your-API/4-3-AnyOrg/AppCreationScripts/Configure.ps1
+++ 4-WebApp-your-API/4-3-AnyOrg/AppCreationScripts/Configure.ps1
@@ -347,7 +347,7 @@ Function ConfigureApplications
     Write-Host "IMPORTANT: Please follow the instructions below to complete a few manual step(s) in the Azure portal":
     Write-Host "- For service"
     Write-Host "  - Navigate to $servicePortalUrl"
-    Write-Host "  - Navigate to the API Permissions page and select 'Grant admin consent for (your tenant)'" -ForegroundColor Red 
+    Write-Host "  - Navigate to the API Permissions page and select 'Grant admin consent for (your tenant)' to User.Read.All permission" -ForegroundColor Red 
     Write-Host "  - Open Service's appsettings.json and update 'AllowedTenants' with your tenant id." -ForegroundColor Red 
     Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------" 
        if($isOpenSSL -eq 'Y')
