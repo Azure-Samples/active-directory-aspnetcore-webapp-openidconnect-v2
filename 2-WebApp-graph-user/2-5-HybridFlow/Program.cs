fdce247e4c647a342f45271696a847caf29911f1
Michael Mitchell-Halter <v-michaelmi@microsoft.com>
2022-04-18 16:33:24 -0700

    Added comments for main program explaining custom flow

--- 2-WebApp-graph-user/2-5-HybridFlow/Program.cs
+++ 2-WebApp-graph-user/2-5-HybridFlow/Program.cs
@@ -22,6 +22,9 @@ builder.Services.AddSession();
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(options =>
     {
+        // Needed to make PKCE possible.
+        //
+        // https://datatracker.ietf.org/doc/html/rfc7636 
         options.ResponseType = OpenIdConnectResponseType.Code;
 
         // Scopes need to be added in to get proper claims for user.
@@ -32,8 +35,21 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             options.Scope.Add(scope);
         }
 
+        // This part of the flow needs to be customized for a hybrid flow. In most flows the OnAuthorizationCodeReceived 
+        // receives an authorization code from the authorization end point and exchanges it for an access token.
+        // This event will still request an access token when it receives an authorization code but it will also
+        // request an access code that will be passed to the front-end so it can be exchanged for an access token.
+        // The access code is passed to the front-end via the 'Microsoft.Identity.Hybrid.Authentication' session value.
         options.Events.OnAuthorizationCodeReceived = async context =>
         {
+            // The 'code_verifier' is an automatically generated value that is used to obtain the auth code. In this
+            // case, this value has already been used to retrieve an auth code and must be used again to redeem the
+            // server auth code for its access token.
+            //
+            // See the 'code_verifier' query parameter at these links for further reading.
+            //
+            // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow#request-an-access-token-with-a-client_secret/
+            // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow#request-an-access-token-with-a-certificate-credential
             context.TokenEndpointRequest.Parameters.TryGetValue("code_verifier", out var codeVerifier);
 
             if (string.IsNullOrEmpty(codeVerifier))
@@ -41,6 +57,12 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 throw new Exception("Unable to retrive verify code challenge.");
             }
 
+            // Exchange the auth code for an access token and client-side auth code.
+            //
+            // The code used in the front-end to exchange for an authentication token is called the 'SpaAuthCode' which
+            // is passed along in the 'Microsoft.Identity.Hybrid.Authentication' value.
+            //
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.identity.client.authenticationresult?view=azure-dotnet
             var authResult = await confidentialClientService.GetAuthenticationResultAsync(options.Scope, context.ProtocolMessage.Code, codeVerifier);
 
             context.Request.HttpContext.Session.SetString("Microsoft.Identity.Hybrid.Authentication", authResult.SpaAuthCode);
